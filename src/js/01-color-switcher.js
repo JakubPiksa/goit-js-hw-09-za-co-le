08df5e1943782d199026cfbdd4e7350df8df1051
Jakub Piksa <kuba.piksa@wp.pl>
2023-05-09 21:54:03 +0200

    1st bit done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+
+console.log(`hi guys`)
+
+
+
+
+
+
+
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
