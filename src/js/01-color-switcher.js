d8bcd6720e6e36a07fe25a4a8cbb393006bac69c
Jakub Piksa <kuba.piksa@wp.pl>
2023-05-09 22:16:58 +0200

    color swicher DZIAŁA zostawiam jak jest bo dziła

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,32 @@
+const body = document.querySelector('body');
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
+let changeColor; 
 
 console.log(`hi guys`)
-
-
-
-
-
-
-
-
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+function startButtonClick() {
+    startButton.disabled = true; 
+    stopButton.disabled = false; 
+    changeColor = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopButtonClick() {
+  startButton.disabled = false; 
+  stopButton.disabled = true;
+  clearInterval(changeColor); 
+}
+
+startButton.addEventListener('click', startButtonClick);
+stopButton.addEventListener('click', stopButtonClick);
+
+
 
