cfc72a7c7e365cb9328ddf768cf7c378d1f0e8f2
Jakub Piksa <kuba.piksa@wp.pl>
2023-05-10 21:58:21 +0200

    nie działa

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,4 @@
-import flatpickr from "flatpickr";
-import Notiflix from "notiflix";
-import "flatpickr/dist/flatpickr.min.css";
+console.log(`hi`)
 
+let x = 5;
+console.log("Wartość x wynosi: " + x); // wyświetla "Wartość x wynosi: 5" w konsoli
